d759afbf341d38a7e76572c8251f5abac5515e79
Coding4Fun44 <121061118+Coding4Fun44@users.noreply.github.com>
2024-01-18 17:01:39 -0500

    Update CalcScript.js

--- CalcScript.js
+++ CalcScript.js
@@ -1,16 +1,16 @@
 let n = [];
 let d = [];
+let s = 0;
 let total1 = '0';
 let total2 = '0';
 let total3 = 0;
-let sign = '';
-let digits = '';
 
 function output(a) {
+    if (total1.length <= 16) {
     if (total1.substring(total1.length - 1, total1.length) === '+' || total1.substring(total1.length - 1, total1.length) === '–' || total1.substring(total1.length - 1, total1.length) === 'x' || total1.substring(total1.length - 1, total1.length) === '÷') {
         d = [];
     }
-    if (total1 === '0' || total3 != 0) {                 
+    if (total1 === '0' || total3 != 0 || total1 === 'Invalid Syntax') {                 
         total1 = a;
         total2 = a;   
         document.getElementById("output").innerHTML = total1;
@@ -27,9 +27,12 @@ function output(a) {
         document.getElementById("output").innerHTML = total1;
         }
         n = [];
+    }
 }
 
+
 function outputplus() {
+    if (total1.length <= 16) {
 if (total1.substring(total1.length-1,total1.length) != '.' && total1.substring(total1.length-1,total1.length) != '-' && total1.substring(total1.length-1, total1.length) != '^') {
     if ((total1.substring(total1.length-1,total1.length) === '+') || (total1.substring(total1.length-1,total1.length) === '–') || (total1.substring(total1.length-1,total1.length) === 'x') || (total1.substring(total1.length-1,total1.length) === '÷')) {
         total1 = total1.substring(0, total1.length-1) + '+';
@@ -59,8 +62,10 @@ else {
 }
 total3 = 0;
 }
+}
 
 function outputminus() {
+    if (total1.length <= 16) {
 if (total1.substring(total1.length-1,total1.length) != '.' && total1.substring(total1.length-1,total1.length) != '-' && total1.substring(total1.length-1, total1.length) != '^') {
     if ((total1.substring(total1.length-1,total1.length) === '+') || (total1.substring(total1.length-1,total1.length) === '–') || (total1.substring(total1.length-1,total1.length) === 'x') || (total1.substring(total1.length-1,total1.length) === '÷')){
         total1 = total1.substring(0, total1.length-1) + '–';
@@ -90,8 +95,10 @@ else {
 }
 total3 = 0;
     }
+}
 
 function outputmult() {
+    if (total1.length <= 16) {
 if (total1.substring(total1.length-1,total1.length) != '.' && total1.substring(total1.length-1,total1.length) != '-' && total1.substring(total1.length-1, total1.length) != '^') {
     if ((total1.substring(total1.length-1,total1.length) === '+') || (total1.substring(total1.length-1,total1.length) === '–') || (total1.substring(total1.length-1,total1.length) === 'x') || (total1.substring(total1.length-1,total1.length) === '÷')){
         total1 = total1.substring(0, total1.length-1) + 'x';
@@ -120,8 +127,10 @@ else {
 }
 total3 = 0;
     }
+}
 
 function outputdiv() {
+    if (total1.length <= 16) {
 if (total1.substring(total1.length-1,total1.length) != '.' && total1.substring(total1.length-1,total1.length) != '-' && total1.substring(total1.length-1, total1.length) != '^') {
     if ((total1.substring(total1.length-1,total1.length) === '+') || (total1.substring(total1.length-1,total1.length) === '–') || (total1.substring(total1.length-1,total1.length) === 'x') || (total1.substring(total1.length-1,total1.length) === '÷')){
         total1 = total1.substring(0, total1.length-1) + '÷';
@@ -151,22 +160,38 @@ if (total1.substring(total1.length-1,total1.length) != '.' && total1.substring(t
 }
 total3 = 0;
     }
+}
 
 function outputequals() {
+    
     if (total1.substring(total1.length-1, total1.length) === ')') {
     total2 = total2.substring(0, total2.length-1);
     }
     if (total2.substring(total2.length-8, total2.length) === 'Math.PI*') {
         total2 = total2.substring(0, total2.length-1);
         }
-    total3 = eval(total2);
-    document.getElementById("output").innerHTML = total3.toString();
+    try {
+         total3 = eval(total2);
+    } catch (error) {
+         total1 = 'Invalid Syntax';
+         document.getElementById("output").innerHTML = total1;
+         total1 = '0';
+         total2 = '0';
+         total3 = 0;
+         n = [];
+         d = [];  
+         s = 1;
+         return;      
+        }
     if ((total3.toString()).includes('.')) {
         d.push('d');
     }
+    if (typeof total3 === 'number') {
+    document.getElementById("output").innerHTML = total3.toString();
     total1 = total3.toString();
     total2 = total3.toString();
     n = [];
+    }
 }
 
 function outputclear() {
@@ -179,7 +204,7 @@ function outputclear() {
 }
 
 function outputbackspace() {
-if (total3 != 0) {
+if (total3 != 0 || s === 1) {
     total1 = '0';
     total2 = '0';
     total3 = 0;
@@ -251,6 +276,7 @@ else if (total1 != '0' && total1.length > 1) {
 }
 
 function outputdecimal() {
+    if (total1.length <= 17) {
     if (d[0] != 'd' ) {
     if (total1.substring(total1.length-1,total1.length) != '.' && total1.substring(total1.length-1,total1.length) != 'π') {
     total1 = total1 + '.'; 
@@ -268,8 +294,10 @@ else if (total1.substring(total1.length - 1, total1.length) === '+' || total1.su
     total3 = 0;
 }
 }
+}
 
 function outputnegative() {
+    if (total1.length <= 17) {
 if (total1.substring(total1.length-1, total1.length) != '.') {
     if (total1 === '0') {
         total1 = '-';
@@ -314,9 +342,11 @@ if (total1.substring(total1.length-1, total1.length) != '.') {
 }
 total3 = 0;
 }
+}
 
 function outputexponent() {
-    if (total1.substring(total1.length-1, total1.length) != '^' && total1.substring(total1.length-1, total1.length) != '.' && total1.substring(total1.length-1, total1.length) != '-' && total1.substring(total1.length-1, total1.length) != '(' && total1.substring(total1.length - 1, total1.length) === '+' && total1.substring(total1.length - 1, total1.length) === '–' && total1.substring(total1.length - 1, total1.length) === 'x' && total1.substring(total1.length - 1, total1.length) === '÷') {
+    if (total1.length <= 16) {
+    if (total1.substring(total1.length-1, total1.length) != '^' && total1.substring(total1.length-1, total1.length) != '.' && total1.substring(total1.length-1, total1.length) != '-' && total1.substring(total1.length-1, total1.length) != '(' && total1.substring(total1.length - 1, total1.length) != '+' && total1.substring(total1.length - 1, total1.length) != '–' && total1.substring(total1.length - 1, total1.length) != 'x' && total1.substring(total1.length - 1, total1.length) != '÷') {
         if (total1.substring(total1.length-1, total1.length) === ')') {
             total1 = total1 + '^';
             total2 = total2.substring(0, total1.length-1) + '**';
@@ -341,8 +371,10 @@ function outputexponent() {
     }
     total3 = 0;
 }
+}
 
 function outputpi() {
+    if (total1.length <= 16) {
 if (total1.substring(total1.length - 1, total1.length) != '.') {
     if (total1 === '0') {                 
         total1 = 'π';
@@ -373,8 +405,10 @@ if (total1.substring(total1.length - 1, total1.length) != '.') {
     }
     total3 = 0;
 }
+}
 
 function outputleftp() {
+    if (total1.length <= 16) {
     if (total1 === '0') {
         total1 = '(';
         total2 = '(';
@@ -395,8 +429,10 @@ function outputleftp() {
     n = [];
     total3 = 0;
 }
+}
 
 function outputrightp() {
+    if (total1.length <= 16) {
 if (total1 != '0') {
     if(total1.substring(total1.length-1, total1.length) != '.' && total1.substring(total1.length-1, total1.length) != '^') {
         total1 = total1 + ')';
@@ -406,6 +442,7 @@ if (total1 != '0') {
     n = [];
 }
 total3 = 0;
+}
 }
 
 function outputpercent() {
